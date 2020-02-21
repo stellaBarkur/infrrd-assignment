877ec9a3c20fe64b37213d4f27d2ecf029a84a0b
unknown <stella.barkur@gmail.com>
2020-02-21 17:00:45 +0530

    complete assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CountryService } from './country.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'infrrd-assignment';
+  public countries;
+  public details = {};
+  public compareDetails = {};
+  public firstSelection = 0;
+  public compare: boolean;
+  public showModal: boolean;
+  constructor(public countryService: CountryService) {
+  }
+
+  ngOnInit() {
+    this.countryService.getCountries().subscribe(
+      res => {
+        this.countries = res;
+        console.log(this.countries);
+      }
+    )
+  }
+
+  public saveCode(e): void {
+    this.firstSelection++;
+    this.showModal = true;
+    let name = e.target.value;
+    if (this.firstSelection > 1) {
+      this.compareDetails = this.details;
+    }
+    this.details = this.countries.filter(x => x.name === name)[0];
+    console.log(this.details);
+  }
+
+  public decideToCompare(decision) {
+    this.showModal = false;
+    this.compare = decision;
+
+  }
 }
--- src/app/country.service.ts
+++ src/app/country.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   public getCountries() {
-    
-
+      const url = 'https://restcountries.eu/rest/v2/all';
+      return this.http.get(url);
   }
 }
